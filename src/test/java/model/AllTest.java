96a7c11057ea0210c7c8479bb8748af4936a1f9f
Matvii Ustich <matvejustic@gmail.com>
2025-03-18 21:53:26 +0000

    Add tests structure

---
+++ src/test/java/model/AllTest.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Includes all test for the actual AI model
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		TestForAIs.class
+})
+public class AllTest {}
