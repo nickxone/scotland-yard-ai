21c591454f6114d210a3cddc03c037fb7a900d41
Matvii Ustich <matvejustic@gmail.com>
2025-03-20 13:41:40 +0000

    Detectives now make reasonable moves

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/AI/Detectives/DetectivesAI.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/AI/Detectives/DetectivesAI.java
@@ -3,6 +3,7 @@ package uk.ac.bris.cs.scotlandyard.ui.ai.AI.Detectives;
 import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.Ai;
 import uk.ac.bris.cs.scotlandyard.model.Board;
+import uk.ac.bris.cs.scotlandyard.model.LogEntry;
 import uk.ac.bris.cs.scotlandyard.model.Move;
 import uk.ac.bris.cs.scotlandyard.ui.ai.model.AIGameState.AIGameState;
 import uk.ac.bris.cs.scotlandyard.ui.ai.model.AIGameState.AIGameStateFactory;
@@ -15,6 +16,7 @@ import java.util.concurrent.TimeUnit;
 
 public class DetectivesAI implements Ai {
     private List<Move> moves;
+    private int MrXLocation;
 
     @Nonnull
     @Override
@@ -26,22 +28,32 @@ public class DetectivesAI implements Ai {
     public void onStart() {
         Ai.super.onStart();
         moves = new ArrayList<>();
+        MrXLocation = 114; // Central location of Mr.X
     }
 
     @Nonnull
     @Override
     public Move pickMove(@Nonnull Board board, Pair<Long, TimeUnit> timeoutPair) {
         if (moves.isEmpty()) {
-            int MrXLocation;
-            MrXLocation = 114; // Central location of Mr.X
+            List<LogEntry> MrXLog = board.getMrXTravelLog().stream().toList();
+            if (MrXLog.get(MrXLog.size() - 1).location().isPresent()) {
+                MrXLocation = MrXLog.get(MrXLog.size() - 1).location().get();
+            }
 
             AIGameStateFactory aiGameStateFactory = new AIGameStateFactory();
             AIGameState gameState = (AIGameState) aiGameStateFactory.build(board, MrXLocation, false);
 
             DetectiveTreeNode root = new DetectiveTreeNode(gameState, null, MrXLocation);
-            moves = root.minimax(2, Integer.MIN_VALUE, Integer.MAX_VALUE, false, 6).getMoves();
+            moves = root.minimax(3, Integer.MIN_VALUE, Integer.MAX_VALUE, false, 6).getMoves();
         }
 
+//        for (Move move : moves) {
+//            for (Move move1 : moves) {
+//                if (!move.equals(move1) && move.commencedBy().webColour().equals(move1.commencedBy().webColour())) {
+//                    System.out.println("a;sdf");
+//                }
+//            }
+//        }
         return moves.remove(0);
     }
 }
